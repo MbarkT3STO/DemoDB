37f2ff1ac68d30a8b43e5dc5c3716df99d4f777a
M B A R K <mbark1236@gmail.com>
2024-02-13 09:09:35 +0100

    Initial comit

---
+++ Tables/dbo.Customers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customers] (
+  [ID] [int] IDENTITY,
+  [FullName] [varchar](50) NOT NULL,
+  [Country] [varchar](50) NOT NULL,
+  [City] [varchar](50) NOT NULL,
+  [Age] [tinyint] NOT NULL,
+  [Job] [varchar](50) NOT NULL
+)
+ON [PRIMARY]
+GO
