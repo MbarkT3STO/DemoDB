2772de198d26413b18b7ecf2e03373da5bfabd07
M B A R K <mbark1236@gmail.com>
2024-02-13 09:11:49 +0100

    pk added

--- Tables/dbo.Customers.sql
+++ Tables/dbo.Customers.sql
@@ -4,7 +4,8 @@
   [Country] [varchar](50) NOT NULL,
   [City] [varchar](50) NOT NULL,
   [Age] [tinyint] NOT NULL,
-  [Job] [varchar](50) NOT NULL
+  [Job] [varchar](50) NOT NULL,
+  CONSTRAINT [PK_Customers_ID] PRIMARY KEY CLUSTERED ([ID])
 )
 ON [PRIMARY]
 GO
